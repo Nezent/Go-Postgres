2fdb1d84adc9334ee8b1ff41bdabb724b84c4981
Sirajum Munir <nezentbd@gmail.com>
2024-03-14 13:42:02 +0600

    1st Commit

--- controller/user.go
+++ controller/user.go
@@ -6,12 +6,15 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// Fetching User Data
 func GetUser(c *gin.Context){
 	users := []models.User{}
 	config.DB.Find(&users)
 	c.JSON(200, &users)
 }
 
+
+// Creating New User
 func CreateUser(c *gin.Context){
 	var user models.User
 	c.BindJSON(&user)
@@ -19,6 +22,8 @@ func CreateUser(c *gin.Context){
 	c.JSON(200,&user)
 }
 
+
+// Deleting User based on ID
 func DeleteUser(c *gin.Context){
 	var user models.User
 	c.BindJSON(&user)
@@ -26,6 +31,8 @@ func DeleteUser(c *gin.Context){
 	c.JSON(200,&user)
 }
 
+
+// Updating User based on ID
 func UpdateUser(c *gin.Context){
 	var user models.User
 	config.DB.Where("ID = ?", c.Param("id")).First(&user)
